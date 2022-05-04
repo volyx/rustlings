2b242754f0fbdc1e00f44b151d895c76e83c2a26
volyx <volyihin@gmail.com>
2022-05-04 09:08:22 +0300

    first commit

---
+++ exercises/variables/variables1.rs
@@ -0,0 +1,9 @@
+// variables1.rs
+// Make me compile!
+// Execute the command `rustlings hint variables1` if you want a hint :)
+
+
+fn main() {
+    let x: i32 = 5;
+    println!("x has the value {}", x);
+}
